9686d5b015442cc447827c2237c044ba2e362756
Karim Ramadan <karim.r.emam@gmail.com>
2026-04-29 09:44:58 +0200

    Removed code

--- java_code/TS/Utils/Loading/AVLoadingIndicatorView$$ExternalSyntheticApiModelOutline0.java
+++
@@ -1,5 +0,0 @@
-package it.tecnosystemi.TS.Utils.Loading;
-
-/* compiled from: D8$$SyntheticClass */
-public final /* synthetic */ class AVLoadingIndicatorView$$ExternalSyntheticApiModelOutline0 {
-}
--- java_code/TS/Utils/NotificationSettings.java
+++
@@ -1,7 +0,0 @@
-package it.tecnosystemi.TS.Utils;
-
-public class NotificationSettings {
-    public static String HubConn = "Endpoint=sb://ProAirPushNamespace.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=aSwewGOX2G6QA4rsjXwKFE7ajdUDlrQhw43vPXn3HLQ=";
-    public static String HubName = "proairmultizone";
-    public static String SenderId = "proairmultizone";
-}
